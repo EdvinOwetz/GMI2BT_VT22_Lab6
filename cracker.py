9bfc5a1333eca62af74df155980d952b397b4909
Nisslett <Nisslett@users.noreply.github.com>
2022-03-17 17:27:55 +0100

    fixed
    Co-Authored-By: EdvinOwetz <76057126+EdvinOwetz@users.noreply.github.com>
    Co-Authored-By: Moooft <100855114+Moooft@users.noreply.github.com>

--- cracker.py
+++ cracker.py
@@ -2,24 +2,25 @@
 
 import multiprocessing
 import concurrent.futures
+import secrets
 import psutil
 from random import randint
 import time
 
 #import sys # endast för sys.maxsize() max av en int
 
-global STAR_VALUE
+global START_VALUE
 
-STAR_VALUE=4294967 # 22 sec @ 8 cores
+START_VALUE=4294967 # 22 sec @ 8 cores
 
 
 
 # __init__ global variables for processes
-def init_globals(key_not_found):
+def init_globals(key_not_found,sec_key):
     global KEY_NOT_FOUND
     KEY_NOT_FOUND = key_not_found
     global HIDDEN_KEY
-    HIDDEN_KEY = randint(1, MAX_INT)
+    HIDDEN_KEY = sec_key
 
 
 
@@ -28,7 +29,7 @@ def crack_something(cpu, cur_key, end_key):
     print(f'CPU: {cpu} keyspace start at {cur_key} and end at {end_key}')
     while KEY_NOT_FOUND.value and (cur_key <= end_key):
         
-        if cur_key == HIDDEN_KEY:
+        if cur_key == HIDDEN_KEY.value:
             KEY_NOT_FOUND.value=False
             return f"CPU: {cpu} found secret key: {cur_key}"
         cur_key+=1
@@ -39,30 +40,29 @@ def main():
     # share variable between processes, notice large V in .Value
     key_not_found = multiprocessing.Value('i', True)
     
+    secret_key = multiprocessing.Value('i',randint(1, START_VALUE))
+    
+
     # definera listor
     cpu_count =  psutil.cpu_count(logical=True)# int
     print("CPU (process) count is: " , cpu_count)
-    #cpus = [i for i in cpu_count] # lista
     cpus = []
     for i in range(cpu_count):
         cpus.append(i)
     print("CPU (process) list is: " , cpus)
-    start_keys= []
+    start_keys=[]
     for i in range(cpu_count):
-        start_keys.append(int((i)*(STAR_VALUE/cpu_count)))
+        start_keys.append(int((i)*(START_VALUE/cpu_count)))
     print("Start keyspace offsets: " , start_keys)
     
     end_keys=list(start_keys)
     end_keys.remove(end_keys[0])
-    end_keys.append(STAR_VALUE)
+    end_keys.append(START_VALUE)
     print("End keyspace offsets: " , end_keys)
-        
-        
-    # [0, 1073741823, 2147483646, 3221225469]
     
-    # [1073741823, 2147483646, 3221225469, 4294967295]
+    print(f"Secret key: {secret_key.value}")
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers = cpu_count, initializer = init_globals, initargs = (key_not_found,)) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers = cpu_count, initializer = init_globals, initargs = (key_not_found,secret_key)) as executor:
         for result in executor.map(crack_something, cpus, start_keys, end_keys):
             print(result)
 
