f0d9363146b73ada2401678acb7da7b81ecddcd5
EdvinOwetz <edvinowetz@gmail.com>
2022-03-21 14:32:17 +0100

    Continued development, added cascade - debugging deluxe
    Co-authored-by: Moooft <Moooft@users.noreply.github.com>
    Co-authored-by: Nisslett <Nisslett@users.noreply.github.com>

--- face_req.py
+++ face_req.py
@@ -1,4 +1,47 @@
 # Face recognition
-
 import os
+from os import path, getcwd
 import cv2 as cv
+import numpy as np
+
+dirpath = getcwd()
+print("dirpath=", dirpath)
+trainingfolder = r"\Faces\training"
+validationfolder = r"\Faces\validation"
+print("trainingfolder=", trainingfolder)
+print("validationfolder=", validationfolder)
+
+person_names = []
+for person_name in os.listdir(dirpath+trainingfolder):
+    person_names.append(person_name)
+    
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+def face_req():
+    face_features = []
+    labels = []
+    
+    #working
+    for person in person_names:
+        print(person)
+        cur_path = trainingfolder.replace("\\","/")+"/"+person
+        print(cur_path)
+        for image_name in os.listdir(dirpath+cur_path):
+            #print(image_name)
+            image_path= cur_path+"/"+image_name
+            #image_path = image_path
+            print(image_name)
+            print(image_path)
+            image_data_raw=cv.imread(image_path.replace("/","//"))
+            #make in monochrome (svart vit)            
+            image_data = cv.cvtColor(image_data_raw,cv.COLOR_RGB2GRAY)
+            #cascade ? aka get the face!
+            #face_recognition=
+            face_recognition = face_cascade.detectMultiScale(image_data, scaleFactor=1.1, minNeighbors=6)
+            for (x, y, w, h) in face_recognition:
+                face_area = image_data[y:y + h, x:x + w]
+                print(face_area)
+                
+            
+            
+face_req()
