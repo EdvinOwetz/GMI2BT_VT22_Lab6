712905b714d581de2ac13f25589de46dc071a2f9
EdvinOwetz <edvinowetz@gmail.com>
2022-03-21 22:17:42 +0100

    Very close with facerecognition, master
    Co-authored-by: Moooft <Moooft@users.noreply.github.com>
    Co-authored-by: Nisslett <Nisslett@users.noreply.github.com>

--- face_req.py
+++ face_req.py
@@ -19,12 +19,15 @@ face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 def face_req():
     face_features = []
-    labels = []
+    person_indexs = []
     
     #working
     for person in person_names:
         print(person)
         cur_path = trainingfolder+"\\"+person
+        
+        person_id=person_names.index(person)
+        
         print(cur_path)
         for image_name in os.listdir(dirpath+"\\"+cur_path):
             image_path= cur_path +"\\"+image_name
@@ -34,12 +37,48 @@ def face_req():
             #cascade ? aka get the face!
             face_recognition = face_cascade.detectMultiScale(image_data, scaleFactor=1.1, minNeighbors=6)
             for (x, y, w, h) in face_recognition:
-                face_area = image_data[y:y + h, x:x + w]
+                face_data = image_data[y:y + h, x:x + w]
+                face_features.append(face_data)
+                person_indexs.append(person_id)
             # Save data in features (facearea)
+    #save here
+    # convert labels list as numpy same with feature list.
+    face_features = np.array(face_features,dtype="object")
+    person_indexs = np.array(person_indexs)
+    
+    face_recognizer= cv.face.LBPHFaceRecognizer_create()
+    
+    # Save traning data into trainer.yml
+    face_recognizer.train(face_features, person_indexs)
+    face_recognizer.save('surleywonderfulpeople.yml')
             # Handle labels, name of person on picture.
-# convert labels list as numpy same with feature list.
-# Save traning data into trainer.yml
-# Validate the training data on validation pictures.
-# Show each result and confidence of the result.     
+
+
+def face_validation():
+    # Validate the training data on validation pictures.
+    face_recognizer=cv.face.LBPHFaceRecognizer_create()
+    face_recognizer.read('surleywonderfulpeople.yml')
+    #loop though images
+    validation_image=list(os.listdir(dirpath+"\\"+validationfolder))
+    for image in validation_image:
+        image_path=validationfolder+"\\"+image
+        image_data_raw=cv.imread(image_path)
+        #make in monochrome (svart vit)            
+        image_data = cv.cvtColor(image_data_raw,cv.COLOR_RGB2GRAY)
+        # finds the face within the image
+        face_recognition = face_cascade.detectMultiScale(image_data, scaleFactor=1.1, minNeighbors=6)
+        
+        for (x, y, w, h) in face_recognition:
+                face_data = image_data[y:y + h, x:x + w]
+                person_id, certainty = face_recognizer.predict(face_data)
+                print(f"Image{validation_image.index(image)}[{image}] is {person_names[person_id]} with {certainty}% certainty. ")
+        
+        
+        
+
+    
+        
+# Show each result and confidence of the result.
             
 face_req()
+face_validation()
