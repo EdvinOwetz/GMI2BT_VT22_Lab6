923af1299cd78719f87a027c65d872d22dfb5c27
Nisslett <Nisslett@users.noreply.github.com>
2022-03-21 15:09:39 +0100

    fixed path  ****

--- face_req.py
+++ face_req.py
@@ -6,13 +6,13 @@ import numpy as np
 
 dirpath = getcwd()
 print("dirpath=", dirpath)
-trainingfolder = r"\Faces\training"
-validationfolder = r"\Faces\validation"
+trainingfolder = "Faces\\training"
+validationfolder = "Faces\\validation"
 print("trainingfolder=", trainingfolder)
 print("validationfolder=", validationfolder)
 
 person_names = []
-for person_name in os.listdir(dirpath+trainingfolder):
+for person_name in os.listdir(dirpath+"\\"+trainingfolder):
     person_names.append(person_name)
     
 face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -24,15 +24,19 @@ def face_req():
     #working
     for person in person_names:
         print(person)
-        cur_path = trainingfolder.replace("\\","/")+"/"+person
+        cur_path = trainingfolder+"\\"+person
         print(cur_path)
-        for image_name in os.listdir(dirpath+cur_path):
+        for image_name in os.listdir(dirpath+"\\"+cur_path):
             #print(image_name)
-            image_path= cur_path+"/"+image_name
+            #image_path= cur_path+"/"+image_name
+            image_path= cur_path +"\\"+image_name
             #image_path = image_path
             print(image_name)
             print(image_path)
-            image_data_raw=cv.imread(image_path.replace("/","//"))
+            image_data_raw=cv.imread(image_path)
+            # if image_data_raw==None :
+            #     print("FUCK there is no image!")
+            #     continue
             #make in monochrome (svart vit)            
             image_data = cv.cvtColor(image_data_raw,cv.COLOR_RGB2GRAY)
             #cascade ? aka get the face!
